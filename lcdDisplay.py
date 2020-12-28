9b58fd3227bd80544b66b718352e19f6d4181f5f
Joern Karthaus <joern.karthaus@gmail.com>
2020-12-28 21:26:10 +0100

    Add many improvements

--- lcdDisplay.py
+++ lcdDisplay.py
@@ -1,30 +1,33 @@
 #!/usr/bin/env python
-import lcddriver 
-from time import *
-import pika
-import sys
-from threading import Thread
-import threading
-import time
 import logging
 import os
+import sys
+import threading
+import time
+import traceback
+from time import *
+
+import pika
+
+import lcddriver
 
 lcd = None
-
 
 rabbitMqHost = os.environ['RABBIT_MQ_HOST']
 rabbitMqQueue = os.environ['RABBIT_MQ_QUEUE']
 demoMode = eval(os.environ.get('DEMO_MODE', False))
 
-rowData = [" "," "," "," "," "]
+rowData = [" ", " ", " ", " ", " "]
 
 connection = pika.BlockingConnection
+
+
 # -------------------------------------------------------------------------------------------------------
 def writeDataToDisplay():
     global lcd
     global rowData
     global demoMode
-    
+
     logging.info("writeDataToDisplay Thread startet...")
     ct = threading.currentThread()
     lcd = lcddriver.lcd()
@@ -36,12 +39,13 @@ def writeDataToDisplay():
 
         rowCount = 0
         for row in rowData:
+            logging.info("Write: " + row + " on LCD row:" + str(rowCount))
             if not demoMode:
                 lcd.lcd_display_string(row, rowCount)
-            else:
-                logging.info("Write: " + row  + " on LCD row:" + str(rowCount))
-            rowCount +=1
+            rowCount += 1
         time.sleep(10)
+
+
 # -------------------------------------------------------------------------------------------------------
 
 
@@ -52,15 +56,19 @@ def openConnection():
 
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitMqHost))
     channel = connection.channel()
-    channel.basic_consume(queue=rabbitMqQueue,on_message_callback=callback)
+    channel.basic_consume(queue=rabbitMqQueue, on_message_callback=callback)
     logging.info("Waiting for Messages on Queue:" + rabbitMqQueue)
     channel.start_consuming()
+
+
 # -------------------------------------------------------------------------------------------------------
 
 
 def closeConnection():
     global connection
     connection.close()
+
+
 # -------------------------------------------------------------------------------------------------------
 
 
@@ -74,15 +82,16 @@ def callback(ch, method, properties, body):
         stringBody = "".join(map(chr, body))
 
         if stringBody.find("=") != -1:
-            if demoMode:
-                logging.info("Rceived:" + stringBody)
+            logging.info("Rceived:" + stringBody)
             row = stringBody[0:stringBody.find("=")]
-            rowData[int(row)] = stringBody[stringBody.find("=")+1:]
+            rowData[int(row)] = stringBody[stringBody.find("=") + 1:]
         else:
             logging.info("Message on Channel: " + rabbitMqQueue +
                          " has an unexpectedly Format -> expecting ROW=DATA")
     except BaseException as error:
         logging.error(error)
+
+
 # -------------------------------------------------------------------------------------------------------
 
 
@@ -96,19 +105,18 @@ def main():
     rowData[2] = " "
     rowData[3] = " "
 
-
     if not demoMode:
         logging.info("LCD initialised...")
         lcd = lcddriver.lcd()
         lcd.lcd_clear()
+
         # Start anf join the output Thread
-        outputThread = Thread(target=writeDataToDisplay)
-        outputThread.join()
+        logging.info("Starting LCD output Thread")
+        outputThread = threading.Thread(target=writeDataToDisplay)
         outputThread.daemon = True
         outputThread.start()
     else:
         logging.info("lcdDisplay Connector Started in DEMO MODE")
-
 
     openConnection()
 
@@ -124,4 +132,5 @@ if __name__ == '__main__':
         logging.info("---------------------------------------------")
         logging.info("-- CRITICAL ERROR OCCURED...")
         logging.info("---------------------------------------------")
+        traceback.print_exc(file=sys.stdout)
         sys.exit(2)
